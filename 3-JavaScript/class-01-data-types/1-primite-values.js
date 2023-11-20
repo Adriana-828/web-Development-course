039d7952c1b28b64868279c573543fb995692e2d
Adriana Sousa <adriana_onaida@hotmail.com>
2023-11-20 14:06:49 -0300

    add folder javascript

---
+++ 3-JavaScript/class-01-data-types/1-primite-values.js
@@ -0,0 +1,43 @@
+console.log("My new Programmer!!!");
+
+// let 123num;
+// let num-1
+// let num 1;
+// let num#1
+let num$1 = 0
+// let númeroç;
+// let if;
+// let break;
+
+console.log(num$1);
+
+// String
+
+let studentName = "Emanuel";
+let age = "30";
+
+console.log(student);
+console.log(typeof student);
+console.log(typeof age);
+
+// Number: Integer, Float, NaN, Infinity
+console.log(typeof 30);
+console.log(typeof 30.5);
+console.log(2 + 3);
+console.log(2 + "3");
+console.log(typeof NaN);
+console.log(typeof Infinity);
+console.log(2 / 0);
+
+let number;
+console.log(number);
+
+// Boolean: true or false
+console.log(typeof false);
+console.log(20 == 20);
+console.log(20 == 30);
+console.log(20 == "20");
+console.log(20 === "20");
+console.log(10 != 10);
+
+
---
+++ 3-JavaScript/class-01-data-types/2-template-literals.js
@@ -0,0 +1,22 @@
+console.log("adriana");
+console.log ("adriana");
+
+
+let course = "Desenvolvimento Web";
+console.log("Turma de " + course); // concatenation
+
+let num1 = 10;
+let num2 = 20;
+
+num1 = Number (num1); // type conversion
+
+console.log(num1 + num2); // type coersion
+// grouping operator
+console.log("A soma de Num1 e Nam2 é " + (num1 + num2));
+
+// interpolation
+console.log("A soma de " + num1 + " e " + num2 + " = " + (num1 + num2));
+
+// templation literals
+let sumNambers = num1 + num2;
+console.log(`A soma de  ${num1} e ${num2} = ${sumNumbers}`);
