16804e2c4ab13619008fba7e3a87de8648866e49
Adriana Sousa <adriana_onaida@hotmail.com>
2023-12-11 14:39:21 -0300

    add class 03 functions

--- 3-JavaScript/class-03-functions/challengs/1-covert-ms-kmh.js
+++ 3-JavaScript/class-03-functions/challengs/1-covert-ms-kmh.js
@@ -1,2 +1,15 @@
+function converTokmh(speedms) {
+    return speedms * 3.6;
+
+}
+
+const userSpeedMs = prompt("Digite a Velocidade em M/s:");
+
+alert(converTokmh (userSpeedMs));
+console.log("test");
+
+
+
+
 
 
